2527f984d3be1e87e9e838d12cb6e55a84e9d1f6
memoryapi <125289931+memoryapi@users.noreply.github.com>
2026-05-16 00:08:08 +0300

    chore: clean up diagnostic scripts from bridge folder

--- lua_bridge/diag_timesig.lua
+++
@@ -1,6 +0,0 @@
--- Diagnostic script to check TimeMap_GetTimeSigAtTime values
-local retval, v1, v2, v3 = reaper.TimeMap_GetTimeSigAtTime(0, 0)
-reaper.ShowConsoleMsg("Retval: " .. tostring(retval) .. "\n")
-reaper.ShowConsoleMsg("V1: " .. tostring(v1) .. "\n")
-reaper.ShowConsoleMsg("V2: " .. tostring(v2) .. "\n")
-reaper.ShowConsoleMsg("V3: " .. tostring(v3) .. "\n")
